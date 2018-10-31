020bba7361e4a9af73c57b05f3e0dd720e3a6759
yanghuan <278432876@qq.com>
2018-10-31 17:28:31 +0800

    动画

---
+++ AnimationDemo/Controller/AniRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  AniRootViewController.h
+//  AnimationDemo
+//
+//  Created by yanghuan on 2018/10/31.
+//  Copyright © 2018 whs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AniRootViewController : UIViewController
+
+@end
---
+++ AnimationDemo/Controller/AniRootViewController.m
@@ -0,0 +1,40 @@
+//
+//  AniRootViewController.m
+//  AnimationDemo
+//
+//  Created by yanghuan on 2018/10/31.
+//  Copyright © 2018 whs. All rights reserved.
+//  
+
+#import "AniRootViewController.h"
+
+@interface AniRootViewController ()
+
+@end
+
+@implementation AniRootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
